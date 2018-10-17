47ab618cf3f90045ac8a93cd5493d0a54a6433ba
Jerry <kickassmusic4vr@hotmail.com>
2018-10-17 01:06:57 -0500

    completed assignment

--- src/components/WizardNine/WizardNine.js
+++ src/components/WizardNine/WizardNine.js
@@ -1,25 +1,62 @@
-import React,  { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import { connect } from "react-redux";
+import {
+  updateAddLineOne,
+  updateAddLineTwo,
+  updateAddLineThree
+} from "../../ducks/reducer";
 
 class WizardNine extends Component {
+  render() {
+    const {
+      updateAddLineOne,
+      updateAddLineTwo,
+      updateAddLineThree
+    } = this.props;
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+          <p>What is your address?</p> <br />
+          <input
+            type="text"
+            placeholder="Line One"
+            onChange={e => updateAddLineOne(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Line Two"
+            onChange={e => updateAddLineTwo(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Line Three"
+            onChange={e => updateAddLineThree(e.target.value)}
+          />
+          <Link to="/wTen">
+            <button className="margin-btn"> Next </button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+function mapStateToProps(state) {
+  const { addressOne, addressTwo, addressThree } = state;
 
-    render(){
-        return(
-            <div className="parent-div">
-                <div className="vert-align">                    
-                    
-                    <p>What is your address?</p> <br />
-
-                    <input type="text" placeholder="Line One" onChange={this.props.updateAddLineOne}/>
-                    <input type="text" placeholder="Line Two" onChange={this.props.updateAddLineTwo}/>
-                    <input type="text" placeholder="Line Three" onChange={this.props.updateAddLineThree}/>
-                
-                    
-                    <Link to="/wTen"><button className="margin-btn"> Next </button></Link>
-                </div>
-            </div>
-        )
-    }
+  return {
+    addressOne,
+    addressTwo,
+    addressThree
+  };
 }
 
-export default WizardNine;
+export default connect(
+  mapStateToProps,
+  {
+    updateAddLineOne,
+    updateAddLineTwo,
+    updateAddLineThree
+  }
+)(WizardNine);
